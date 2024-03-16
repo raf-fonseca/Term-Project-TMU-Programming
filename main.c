e67428d6ee8c3e8a351ec5101e1a944d6f87d12a
SilkyJesus <rafael.silvanofonseca@gmail.com>
2024-03-16 18:45:53 -0400

    test

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
-    
 
     //Q1 calculate the yearly averages for each year between 1760 and 2015 (One avrg per year)
-    
-=======
-    printf("Hello, World!\n");
     return 0;
     printf("Hello, World!\n");
->>>>>>> 74881175989647b45bb64cfebc323383f248f363
 }
